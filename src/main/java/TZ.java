dac6f0cf8c41605681811e6a32b03dc7641631c2
Tristan Tarrant <ttarrant@redhat.com>
2023-11-02 17:30:47 +0100

    Initial commit

---
+++ src/main/java/TZ.java
@@ -0,0 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TZ {
+   static final Logger logger = LogManager.getLogger("HelloWorld");
+
+   public static void main(String[] args) {
+      logger.info("Hello, World!");
+   }
+
+}
