7992d1851a05c7bf8b79ed25b1e7280e7429b3d5
Tristan Tarrant <ttarrant@redhat.com>
2023-11-02 17:38:50 +0100

    Generate heap dump
    Add explanation to date format in pattern layout

--- src/main/java/TZ.java
+++ src/main/java/TZ.java
@@ -1,11 +1,26 @@
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import javax.management.MBeanServer;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.sun.management.HotSpotDiagnosticMXBean;
 
 public class TZ {
    static final Logger logger = LogManager.getLogger("HelloWorld");
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws IOException {
       logger.info("Hello, World!");
+
+      MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+      HotSpotDiagnosticMXBean mxBean = ManagementFactory.newPlatformMXBeanProxy(
+            server, "com.sun.management:type=HotSpotDiagnostic", HotSpotDiagnosticMXBean.class);
+      Files.deleteIfExists(Path.of("tz.hprof"));
+      mxBean.dumpHeap("tz.hprof", false);
    }
 
 }
